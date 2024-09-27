4530ceb107505d6ec1c0d37ce93f90478eb94598
eduardoagabes <eduardoagabes@hotmail.com>
2024-09-27 17:06:25 +0200

    update 2.0

--- app/src/main/java/com/eduardoagabes/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/eduardoagabes/tipscalculator/MainActivity.kt
@@ -3,8 +3,10 @@ package com.eduardoagabes.tipscalculator
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +14,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,47 +25,6 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val edtFactura = findViewById<TextInputEditText>(R.id.edt_factura_total)
-        val edtPersonas = findViewById<TextInputEditText>(R.id.edt_personas)
-
-        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
-        val btnLimpiar = findViewById<Button>(R.id.btn_limpiar)
-
-        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
-
-
-        val limpiarCampo1 = findViewById<EditText>(R.id.edt_factura_total)
-        val limpiarCampo2 = findViewById<EditText>(R.id.edt_personas)
-
-        btnCalcular.setOnClickListener {
-            val facturaStr: String = edtFactura.text.toString()
-            val personasStr: String = edtPersonas.text.toString()
-
-            if (facturaStr == "" || personasStr == "") {
-                Snackbar
-                    .make(
-                        edtFactura,
-                        "Rellena todos los campos",
-                        Snackbar.LENGTH_LONG
-                    )
-                    .show()
-            } else {
-
-                val factura = facturaStr.toFloat()
-                val personas = personasStr.toInt()
-
-
-
-                val intent = Intent(this, ResultActivity::class.java)
-                startActivity(intent)
-            }
-        }
-
-        btnLimpiar.setOnClickListener {
-            limpiarCampo1.text.clear()
-            limpiarCampo2.text.clear()
         }
     }
 }
