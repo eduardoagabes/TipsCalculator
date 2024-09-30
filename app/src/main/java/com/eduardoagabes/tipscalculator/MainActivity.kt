46eab8e5cfba18a26f42b4bc1f7671cfdff25fab
eduardoagabes <eduardoagabes@hotmail.com>
2024-09-30 13:19:40 +0200

    agregando Spinner

--- app/src/main/java/com/eduardoagabes/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/eduardoagabes/tipscalculator/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.eduardoagabes.tipscalculator
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.eduardoagabes.tipscalculator.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -45,19 +51,55 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        binding.btnClean.setOnClickListener {
+        val adapter = ArrayAdapter.createFromResource(
+            this, R.array.num_people,
+            android.R.layout.simple_spinner_item
+        )
 
-        }
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerNumberOfPeople.adapter = adapter
+
+        var numOfPeopleSelected = 0
+        binding.spinnerNumberOfPeople.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    numOfPeopleSelected = position
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+            }
 
         binding.btnCalculate.setOnClickListener {
-            val totalTable: Float = binding.tieTotal.text.toString().toFloat()
-            val nPeople: Int = binding.tieNumPeople.text.toString().toInt()
+            val totalTableTemp = binding.tieTotal.text
 
-            val totalTemp = totalTable / nPeople
-            val tips = totalTemp * percentage / 100
-            val totalWhitTips = totalTable + tips
+            if (totalTableTemp?.isEmpty() == true) {
+                Snackbar.make(binding.tieTotal, "Rellene todos los campos", Snackbar.LENGTH_LONG)
+                    .show()
+
+            } else {
+                val totalTable: Float = totalTableTemp.toString().toFloat()
+                val nPeople: Int = numOfPeopleSelected
+
+                val totalTemp = totalTable / nPeople
+                val tips = (totalTemp * percentage) / 100
+                val totalWhitTips = totalTemp + tips
+                binding.tvResult.text = "Total con propina: $totalWhitTips"
+            }
+
+            binding.btnClean.setOnClickListener {
+                binding.tvResult.text = ""
+                binding.tieTotal.setText("")
+                binding.rbOptionOne.isChecked = false
+                binding.rbOptionTwo.isChecked = false
+                binding.rbOptionThree.isChecked = false
+            }
         }
-
-
     }
 }
